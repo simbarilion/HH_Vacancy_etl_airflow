4528a628cecc876f6f00a9c1ce4d59f234d40ad8
nadezhda <nadezhda13popova@gmail.com>
2026-04-17 17:09:47 +0700

    init

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+from configparser import ConfigParser
+
+from dotenv import load_dotenv
+
+ROOT_DIR = os.path.dirname(__file__)
+
+LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+load_dotenv(os.path.join(ROOT_DIR, ".env"))
+
+
+def config(filename: str = "database.ini", section: str = "postgresql") -> dict:
+    """Возвращает параметры для подключения к БД"""
+    parser = ConfigParser()
+    parser.read(os.path.join(ROOT_DIR, filename), encoding="utf-8")
+
+    if not parser.has_section(section):
+        raise Exception(f"Section {section} is not found in the {filename} file")
+
+    db_config = {key: value.strip() for key, value in parser.items(section)}
+
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    if db_user is None or db_password is None:
+        raise EnvironmentError("Переменная окружения не установлена")
+
+    db_config["user"] = db_user
+    db_config["password"] = db_password
+
+    return db_config
---
+++ src/logging_config.py
@@ -0,0 +1,81 @@
+import inspect
+import logging
+from pathlib import Path
+from typing import Literal, Optional
+
+LogLevel = int | Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+
+class LoggingConfigClassMixin:
+    """
+    Миксин для настройки логеров. Дает возможность настраивать логер с выводом в файл и/или консоль.
+    :name: Имя логгера (обычно __name__)
+    :level: уровень логирования (по умолчанию INFO)
+    :log_file: путь к лог-файлу (если нужно логировать в файл)
+    :log_to_console: по умолчанию не создается (если нужно логировать в консоль)
+    :fmt: формат сообщения,
+    :clear_log_on_start: удаляет старый лог-файл один раз при запуске,
+    """
+
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        level: LogLevel = "INFO",
+        log_file: Optional[str] = None,
+        log_to_console: bool = False,
+        clear_log_on_start: bool = True,
+        fmt: str = "%(asctime)s - %(levelname)s - logger:%(name)s - module:%(module)s "
+        "- func:%(funcName)s:%(lineno)d - %(message)s",
+    ) -> None:
+        """Конструктор для класса логгирования"""
+        self.name = name or self._get_caller_module_name()
+        self.level = level
+        self.log_file = log_file or f"{self.name}.log"
+        self.log_to_console = log_to_console
+        self.clear_log_on_start = clear_log_on_start
+        self.fmt = fmt
+
+    def configure(self) -> logging.Logger:
+        """Возвращает экземпляр логгера с заданной конфигурацией"""
+        logger = logging.getLogger(self.name)
+        logger.setLevel(self.level)
+
+        if logger.hasHandlers():
+            return logger
+
+        formatter = logging.Formatter(self.fmt, datefmt="%Y-%m-%d %H:%M:%S")
+
+        if self.log_to_console:
+            self._add_console_handler(logger, formatter)
+
+        if self.log_file:
+            self._add_file_handler(logger, formatter)
+
+        return logger
+
+    def _add_file_handler(self, logger: logging.Logger, formatter: logging.Formatter) -> None:
+        """Создает Хэндлер для записи логов в файл"""
+        logs_dir = Path(__file__).resolve().parent.parent / "logs"
+        logs_dir.mkdir(parents=True, exist_ok=True)
+        log_path = logs_dir / self.log_file
+
+        if self.clear_log_on_start and log_path.exists():
+            log_path.unlink()
+
+        file_handler = logging.FileHandler(log_path, mode="a", encoding="utf-8")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    @staticmethod
+    def _add_console_handler(logger: logging.Logger, formatter: logging.Formatter) -> None:
+        """Создает Хэндлер для вывода логов в консоль"""
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    @staticmethod
+    def _get_caller_module_name() -> str:
+        """Возвращает имя модуля, вызвавшего конфигурацию логгера"""
+        caller_frame = inspect.stack()[2]
+        caller_module = inspect.getmodule(caller_frame.frame)
+        return caller_module.__name__ if caller_module else "unknown"
